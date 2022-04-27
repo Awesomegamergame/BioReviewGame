3defe3ddb21b04a97937004d1fd9be812575ff81
Awesomegamergame <68134172+awesomegamergame@users.noreply.github.com>
2022-04-26 22:50:32 -0400

    Added an option to add your own json file instead of the prebuilt one.

--- Json.cs
+++ Json.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using static System.Environment;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace BioReviewGame
 {
@@ -15,21 +16,22 @@ namespace BioReviewGame
         public static List<string> a3List = new List<string>();
         public static List<string> a4List = new List<string>();
         public static List<string> cList = new List<string>();
-        public static int JsonReader()
+        public static int JsonReader(string directory)
         {
             int size = 0;
             try
             {
-                dynamic obj = JsonConvert.DeserializeObject<dynamic>(File.ReadAllText($@"{CurrentDirectory}\Data.json"));
+                string jsonFile = new StreamReader(directory).ReadToEnd();
+                dynamic obj = JsonConvert.DeserializeObject<dynamic>(jsonFile);
                 dynamic json = obj.Questions;
                 foreach (JProperty data in json)
                 {
-                    questionlist.Add(ReadJson(data.Name, "question", "Data", "Questions"));
-                    a1List.Add(ReadJson(data.Name, "a1", "Data", "Questions"));
-                    a2List.Add(ReadJson(data.Name, "a2", "Data", "Questions"));
-                    a3List.Add(ReadJson(data.Name, "a3", "Data", "Questions"));
-                    a4List.Add(ReadJson(data.Name, "a4", "Data", "Questions"));
-                    cList.Add(ReadJson(data.Name, "c", "Data", "Questions"));
+                    questionlist.Add(ReadJson(data.Name, "question", jsonFile, "Questions"));
+                    a1List.Add(ReadJson(data.Name, "a1", jsonFile, "Questions"));
+                    a2List.Add(ReadJson(data.Name, "a2", jsonFile, "Questions"));
+                    a3List.Add(ReadJson(data.Name, "a3", jsonFile, "Questions"));
+                    a4List.Add(ReadJson(data.Name, "a4", jsonFile, "Questions"));
+                    cList.Add(ReadJson(data.Name, "c", jsonFile, "Questions"));
                     size++;
                 }
             }
@@ -40,10 +42,43 @@ namespace BioReviewGame
             }
             return size;
         }
-        public static string ReadJson(string GVersion, string Property, string FileName, string TopLevel)
+        public static int JsonReaderEmbed()
         {
-            JObject rss = JObject.Parse(File.ReadAllText($@"{CurrentDirectory}\{FileName}.json"));
+            int size = 0;
+            try
+            {
+                var DataStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("BioReviewGame.Data.json");
+                string jsonEmbed = new StreamReader(DataStream).ReadToEnd();
+                dynamic obj = JsonConvert.DeserializeObject<dynamic>(jsonEmbed);
+                dynamic json = obj.Questions;
+                foreach (JProperty data in json)
+                {
+                    questionlist.Add(ReadJsonEmbed(data.Name, "question", jsonEmbed, "Questions"));
+                    a1List.Add(ReadJsonEmbed(data.Name, "a1", jsonEmbed, "Questions"));
+                    a2List.Add(ReadJsonEmbed(data.Name, "a2", jsonEmbed, "Questions"));
+                    a3List.Add(ReadJsonEmbed(data.Name, "a3", jsonEmbed, "Questions"));
+                    a4List.Add(ReadJsonEmbed(data.Name, "a4", jsonEmbed, "Questions"));
+                    cList.Add(ReadJsonEmbed(data.Name, "c", jsonEmbed, "Questions"));
+                    size++;
+                }
+            }
+            catch (JsonSerializationException ex)
+            {
+                MessageBox.Show($"Something is wrong with the json file.\n\nError Code: {ex}");
+                Application.Current.Shutdown();
+            }
+            return size;
+        }
+        public static string ReadJson(string GVersion, string Property, string json, string TopLevel)
+        {
+            JObject rss = JObject.Parse(json);
             return (string)rss[TopLevel][GVersion][Property];
         }
+        public static string ReadJsonEmbed(string GVersion, string Property, string Embed, string TopLevel)
+        {
+            JObject rss = JObject.Parse(Embed);
+            return (string)rss[TopLevel][GVersion][Property];
+        }
+
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
+using Microsoft.Win32;
 
 namespace BioReviewGame
 {
@@ -6,23 +8,13 @@ namespace BioReviewGame
     {
         public static MainWindow GameWindow;
         public static int QuestionNumber;
-        public static int size;
+        public int size;
         public MainWindow()
         {
             InitializeComponent();
             GameWindow = this;
-            try
-            {
-                size = Json.JsonReader();
-            }
-            catch (System.IO.FileNotFoundException ex)
-            {
-                MessageBox.Show($"A Dll File is missing. Please make sure you have the correct files in the correct folder.\n\nError Code: {ex}");
-                Application.Current.Shutdown();
-            }
-            QuestionNumber = Game.Start(0);
         }
-
+        #region Quiz Buttons
         private void Button1_Click(object sender, RoutedEventArgs e)
         {
             if (Button1.Tag.Equals("correct")) 
@@ -41,7 +33,6 @@ namespace BioReviewGame
             else
                 MessageBox.Show("Wrong answer!");
         }
-
         private void Button2_Click(object sender, RoutedEventArgs e)
         {
             if (Button2.Tag.Equals("correct"))
@@ -60,7 +51,6 @@ namespace BioReviewGame
             else
                 MessageBox.Show("Wrong answer!");
         }
-
         private void Button3_Click(object sender, RoutedEventArgs e)
         {
             if (Button3.Tag.Equals("correct"))
@@ -79,7 +69,6 @@ namespace BioReviewGame
             else
                 MessageBox.Show("Wrong answer!");
         }
-
         private void Button4_Click(object sender, RoutedEventArgs e)
         {
             if (Button4.Tag.Equals("correct"))
@@ -97,6 +86,50 @@ namespace BioReviewGame
             }
             else
                 MessageBox.Show("Wrong answer!");
+        }
+        #endregion
+        private void Embed_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                size = Json.JsonReaderEmbed();
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show($"A Dll Or Data File is missing. Please make sure you have the correct files in the correct folder.\n\nError Code: {ex}");
+                Application.Current.Shutdown();
+            }
+            Embed.Visibility = Visibility.Collapsed;
+            BackgroundP.Visibility = Visibility.Collapsed;
+            Start.Visibility = Visibility.Collapsed;
+            Select.Visibility = Visibility.Collapsed;
+            QuestionNumber = Game.Start(0);
+        }
+        private void Select_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Title = "Choose Questions .json File",
+                Filter = "Json Files|*.json;*.JSON;"
+            };
+            bool? result = openFileDialog.ShowDialog();
+            if(result == true)
+            {
+                try
+                {
+                    size = Json.JsonReader(openFileDialog.FileName);
+                }
+                catch (FileNotFoundException ex)
+                {
+                    MessageBox.Show($"A Dll Or Data File is missing. Please make sure you have the correct files in the correct folder.\n\nError Code: {ex}");
+                    Application.Current.Shutdown();
+                }
+                Embed.Visibility = Visibility.Collapsed;
+                BackgroundP.Visibility = Visibility.Collapsed;
+                Start.Visibility = Visibility.Collapsed;
+                Select.Visibility = Visibility.Collapsed;
+                QuestionNumber = Game.Start(0);
+            }
         }
     }
 }
